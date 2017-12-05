302df374e5f988042306cb3a8be00ed15d0e7c82
mkhaufillah <khaufillahmohammad@gmail.com>
2017-12-05 08:29:35 +0700

    update mayor

--- js/extras-index.js
+++ js/extras-index.js
@@ -1,6 +1,72 @@
 $(document).ready(function(){
   var isMobile = window.screen.width <= 991;
 
+  // get login status
+  if (Cookies.get('usr-id') != null) window.location.replace("beranda");
+
+  // set halaman kategori
+  for (i in category) {
+    var title = "";
+    for (j in category[i]['title']) {
+      title = title + category[i]['title'][j] + ", ";
+    }
+    $('#category').append(
+      "<div class='card card-anim'>" +
+        "<div class='card-image waves-effect waves-block waves-light'>" +
+          "<img class='activator' src='"+category[i]['img-url']+"'>" +
+        "</div>" +
+        "<div class='card-content'>" +
+          "<span class='card-title activator grey-text text-darken-4 center'>"+category[i]['category']+"</span>" +
+        "</div>" +
+        "<div class='card-reveal'>" +
+          "<span class='card-title grey-text text-darken-4 left'>" +
+            category[i]['category']+"<i class='material-icons right'>close</i>" +
+          "</span>" +
+          "<p class='justify-text'>" +
+             title +
+          "</p>" +
+        "</div>" +
+      "</div>"
+    );
+  }
+
+  //validasi
+  $.validator.setDefaults({
+    errorClass: 'invalid',
+    validClass: 'valid',
+    errorPlacement: function (error, element) {
+        $(element)
+            .closest('form')
+            .find("label[for='" + element.attr("id") + "']")
+            .attr('data-error', error.text());
+    },
+    submitHandler: function (form) {
+      var email = form.email.value;
+      var password = form.password.value;
+      var ee = $('#email');
+      var ep = $('#password');
+      for (i in user) {
+        if (user[i].email == email && user[i].password == password) {
+          Cookies.set('usr-id', i);
+          window.location.replace("beranda");
+          return;
+        }
+      }
+      ee.addClass("invalid");
+      ee.closest('form')
+        .find("label[for='email']")
+        .attr('data-error', 'Kombinasi password dan email salah');
+      ep.addClass("invalid");
+      ep.closest('form')
+        .find("label[for='password']")
+        .attr('data-error', 'Kombinasi password dan email salah');
+    }
+  });
+
+  $('#form-login').validate();
+  $('#form-daftar-agent').validate();
+  $('#form-daftar-boy').validate();
+
   //scrolling
   var eb = $('body');
   var enw = $('.nav-wrapper');
---
+++ js/extras-utama-home.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+  // rec komunitas
+  for(i in comunity) {
+    if (comunity[i].talent == user[Cookies.get('usr-id')].talent) {
+      $('.card-app-r-k').append(
+        "<div class='col s12 m6 l6'>" +
+          "<div class='card sticky-action'>" +
+            "<div class='card-image waves-effect waves-block waves-light'>" +
+              "<img class='activator' src='"+comunity[i]['img-url']+"'>" +
+            "</div>" +
+            "<div class='card-content'>" +
+              "<span class='card-title activator grey-text text-darken-4 overflow-text-set'>"+comunity[i].name+"</span>" +
+              "<p class='overflow-text-set'>"+comunity[i].desc+"</p>" +
+            "</div>" +
+            "<div class='card-action'>" +
+              "<a href='#!' class='overflow-text-set'>Bergabung</a>" +
+            "</div>" +
+          "</div>" +
+        "</div>"
+      );
+    }
+  }
+
+  // rec pebakat/pelatih
+  for(i in user) {
+    if (user[i].role != user[Cookies.get('usr-id')].role &&
+        user[i].talent == user[Cookies.get('usr-id')].talent) {
+          $('.card-app-r-p').append(
+            "<div class='col s12 m6 l6'>" +
+              "<div class='card sticky-action'>" +
+                "<div class='card-image waves-effect waves-block waves-light'>" +
+                  "<img class='activator' src='"+user[i]['img-url']+"'>" +
+                "</div>" +
+                "<div class='card-content'>" +
+                  "<span class='card-title activator grey-text text-darken-4 overflow-text-set'>"+user[i].name+"</span>" +
+                  "<p class='overflow-text-set'>"+user[i].talent+"</p>" +
+                "</div>" +
+                "<div class='card-action'>" +
+                  "<a href='#!' class='overflow-text-set'>Kontrak</a>" +
+                "</div>" +
+              "</div>" +
+            "</div>"
+          );
+        }
+  }
+
+  // popular category
+  for(i in categoryIndexPop) {
+    $('.katpop').append(
+      "<a id='"+category[categoryIndexPop[i][0]].title[categoryIndexPop[i][1]]+"' href='../cari' class='collection-item avatar'>"+
+        "<img src='"+categoryIndexPop[i][2]+"' class='circle'>"+
+        "<span class='title overflow-text-set'>"+category[categoryIndexPop[i][0]].category+"</span>"+
+        "<p class='overflow-text-set'>"+category[categoryIndexPop[i][0]].title[categoryIndexPop[i][1]]+"</p>"+
+        "<p class='overflow-text-set'>"+categoryIndexPop[i][3]+" peminat</p>"+
+      "</a>"
+    );
+  }
+
+  // all category
+  for(i in category) {
+    var string = "";
+    for(j in category[i].title) {
+      string = string + "<a id='"+category[i].title[j]+"' class='collection-item overflow-text-set' href='../cari'>"+category[i].title[j]+"</a>";
+    }
+    $('.katall').append(
+      "<li>"+
+        "<div class='collapsible-header overflow-text-set'><i class='material-icons'>poll</i>"+category[i].category+"</div>"+
+        "<div class='collapsible-body'>"+
+          "<ul class='collection'>"+
+            string+
+          "</ul>"+
+        "</div>"+
+      "</li>"
+    );
+  }
+});
